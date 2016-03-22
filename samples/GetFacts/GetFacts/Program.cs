00227da7634b58fe34b7625a064129b065f73a7c
Federico Cavalieri <f@28.io>
2016-03-22 13:16:21 +0100

    Fixed examples

--- samples/GetFacts/GetFacts/Program.cs
+++ samples/GetFacts/GetFacts/Program.cs
@@ -7,34 +7,37 @@ using Newtonsoft.Json;
 
 namespace GetFacts
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            String endpoint = "http://secxbrl.28.io/v1/_queries/public";
-            CellStore.Client.ApiClient client = new CellStore.Client.ApiClient(endpoint);
-            CellStore.Api.DataApi dataAPI = new CellStore.Api.DataApi(client);
-            CellStore.Api.SessionsApi sessionsAPI = new CellStore.Api.SessionsApi(client);
+      String endpoint = "http://secxbrl.28.io/v1/_queries/public";
+      CellStore.Api.DataApi dataAPI = new CellStore.Api.DataApi(endpoint);
 
-            // login .. Alternative step to create a token dynamically
-            //object loginResponse = sessionsAPI.Login("example@email.com", "password");
-            //String token = loginResponse.token;
-            //Console.WriteLine(login.token);
-            String token = "c3049752-4d35-43da-82a2-f89f1b06f7a4";
+      String token = "c3049752-4d35-43da-82a2-f89f1b06f7a4";
+      // login .. Alternative step to create a token dynamically
+      /*
+        CellStore.Api.SessionsApi sessionsAPI = new CellStore.Api.SessionsApi(endpoint);
+        dynamic loginResponse = sessionsAPI.Login("example@email.com", "password");
+        String token = loginResponse["token"];      
+      */
 
-            // list some facts
-            dynamic factsResonse = dataAPI.ListFacts(token, ticker: "t", fiscalYear: "2014", fiscalPeriod: "FY", concept: "us-gaap:Assets");
-            dynamic factTable = factsResonse["FactTable"];
-            int i = 0;
-            foreach (dynamic fact in factTable.Children())
-            {
-                Console.WriteLine("===========================");
-                Console.WriteLine("Fact " + i + ":");
-                Console.WriteLine(JsonConvert.SerializeObject(fact, Formatting.Indented));
-                i++;
-            }
+      // list some facts
+      dynamic factsResonse = dataAPI.ListFacts(token, ticker: new List<string> { "t" }, 
+                                                      fiscalYear: new List<string> { "2014" }, 
+                                                      fiscalPeriod: new List<string> { "FY" }, 
+                                                      concept: new List<string> { "us-gaap:Assets" });
+      dynamic factTable = factsResonse["FactTable"];
+      int i = 0;
+      foreach (dynamic fact in factTable.Children())
+      {
+        Console.WriteLine("===========================");
+        Console.WriteLine("Fact " + i + ":");
+        Console.WriteLine(JsonConvert.SerializeObject(fact, Formatting.Indented));
+        i++;
+      }
 
-            //Console.ReadKey();
-        }
+      Console.ReadKey();
     }
+  }
 }
