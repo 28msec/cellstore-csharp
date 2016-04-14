e35a24b66699ca597e0168f07ea0b4a9f3854277
Dennis Knochenwefel <d@28.io>
2016-04-14 13:48:40 +0200

    :lipstick: cleanup

--- samples/GetFacts/GetFacts/Program.cs
+++ samples/GetFacts/GetFacts/Program.cs
@@ -15,12 +15,12 @@ namespace GetFacts
       CellStore.Api.DataApi dataAPI = new CellStore.Api.DataApi(endpoint);
 
       String token = "c3049752-4d35-43da-82a2-f89f1b06f7a4";
-            // login .. Alternative step to create a token dynamically
-            /*
-              CellStore.Api.SessionsApi sessionsAPI = new CellStore.Api.SessionsApi(endpoint);
-              dynamic loginResponse = sessionsAPI.Login("example@email.com", "password");
-              String token = loginResponse["token"];      
-            */
+      // login .. Alternative step to create a token dynamically
+      /*
+        CellStore.Api.SessionsApi sessionsAPI = new CellStore.Api.SessionsApi(endpoint);
+        dynamic loginResponse = sessionsAPI.Login("example@email.com", "password");
+        String token = loginResponse["token"];      
+      */
 
 
       pepVsCocaColaFacts(dataAPI, token);
